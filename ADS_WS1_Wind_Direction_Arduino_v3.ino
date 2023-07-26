4815985c87f4ce1d14d544afb563725fff648fbd
Manish E S <41136588+esmanish@users.noreply.github.com>
2023-07-26 18:40:49 +0530

    Update ADS_WS1_Wind_Direction_Arduino_v3.ino
    These are Calibrated values.

--- ADS_WS1_Wind_Direction_Arduino_v3.ino
+++ ADS_WS1_Wind_Direction_Arduino_v3.ino
@@ -1,46 +1,48 @@
-#include <Filter.h>
+const int pin = 34;  // Replace with the appropriate pin number for your wind vane
 
-AnalogFilter<100, 0> filter;
+const int numDirections = 16;
 
-const int pin = 34;
+const char* bin[numDirections] = { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" };
 
-const double voltageValues[16] = { 0.63, 0.52, 1.03, 1.01, 1.11, 1.1, 1.07, 1.09, 0.81, 0.88, 0.14, 0.17, 0.09, 0.4, 0.26, 0.0 };
-const char *direction[] = { "N", "NE", "NE", "NE", "E", "SE", "SE", "SE", "S", "SW", "SW", "SW", "W", "NW", "NW", "NW" };
-const double dirDegrees[16] = { 0.0, 22.5, 45.0, 67.5, 90.0, 112.5, 135.0, 157.5, 180.0, 202.5, 225.0, 247.5, 270.0, 292.5, 315.0, 337.5 };
+int getWindDirection() {
+  int rawValue = analogRead(pin);
+  Serial.println(rawValue);
 
-int dir = 0, dirDig = 0, i;
+  int directionIndex = -1;
 
-double round(double value) {
-  return (int)(value * 100 + 0.5) / 100.0;
+  if (rawValue >= 140 && rawValue < 218) directionIndex = 10;    // SW
+  else if (rawValue >= 218 && rawValue < 296) directionIndex = 11;   // WSW
+  else if (rawValue >= 296 && rawValue < 374) directionIndex = 12;   // NNW
+  else if (rawValue >= 374 && rawValue < 452) directionIndex = 13;   // NW
+  else if (rawValue >= 452 && rawValue < 530) directionIndex = 14;   // WNW
+  else if (rawValue >= 530 && rawValue < 608) directionIndex = 15;   // NNE
+  else if (rawValue >= 608 && rawValue < 686) directionIndex = 0;    // N
+  else if (rawValue >= 686 && rawValue < 764) directionIndex = 1;    // W
+  else if (rawValue >= 764 && rawValue < 842) directionIndex = 2;  // SSW
+  else if (rawValue >= 842 && rawValue < 920) directionIndex = 3;  // NE
+  else if (rawValue >= 920 && rawValue < 998) directionIndex = 4;  // S
+  else if (rawValue >= 998 && rawValue < 1076) directionIndex = 5;  // SSE
+  else if (rawValue >= 1076 && rawValue < 1154) directionIndex = 6;  // E
+  else if (rawValue >= 1154 && rawValue < 1232) directionIndex = 7;  // ESE
+  else if (rawValue >= 1232 && rawValue < 1310) directionIndex = 8;  // SE
+  else if (rawValue >= 1310 && rawValue <= 1260) directionIndex = 9;  // ENE
+
+  return directionIndex;
 }
-
-int checkWindDir() {
-
-  int value = analogRead(pin);
-  int filtered = filter.update(value);
-  double voltage = filtered * (3.3 / 4095.0);
-
-  for (i = 0; i <= 14; i++) {
-    if (round(voltage) == voltageValues[i]) {
-      dir = i;
-    }
-    // else {
-    //   Serial.println("Direction not Found!!!");
-    // }
-  }
-  Serial.print("Direction: ");
-  Serial.print(dirDegrees[dir]);
-  Serial.print(" ");
-  Serial.println(direction[dir]);
-
-  return dir + 1;
-}
-
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
-  int Direction = checkWindDir();
+  int directionIndex = getWindDirection();
+
+  if (directionIndex >= 0 && directionIndex < numDirections) {
+    Serial.print("Wind Direction: ");
+    Serial.println(bin[directionIndex]);
+  } else {
+    Serial.println("Direction not found!");
+  }
+
+  delay(1000);  // Delay for 1 second between readings
 }
